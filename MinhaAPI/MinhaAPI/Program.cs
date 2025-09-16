7b65bb8c22ae5e01178ddabf9efadf4858a65cb7
Matheus Pompeo <matheuspompeo13@gmail.com>
2025-09-16 14:49:10 -0300

    adding classes

---
+++ MinhaAPI/MinhaAPI/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container. --> ConfigureServices()
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build(); // --> Configure()
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
