8ec570675e3bd1177a206fa4a92b3ea67a96a3c8
Matheus Pompeo <matheus.pompeo@suntrans.com.br>
2025-10-02 09:33:55 -0300

    using validations

--- APICatalogo/APICatalogo/Models/Produto.cs
+++ APICatalogo/APICatalogo/Models/Produto.cs
@@ -1,35 +1,60 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using APICatalogo.Validations;
 
-namespace APICatalogo.Models;
-
-[Table("Produtos")]
-public class Produto
+namespace APICatalogo.Models
 {
-    [Key]
-    public int ProdutoId { get; set; }
+    [Table("Produtos")]
+    public class Produto : IValidatableObject
+    {
+        [Key]
+        public int ProdutoId { get; set; }
 
-    [Required]
-    [StringLength(80)]
-    public string? Nome { get; set; }
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [StringLength(80, ErrorMessage = "O nome deve ter entre 5 e 80 caracteres", MinimumLength = 5)]
+        // [PrimeiraLetraMaiuscula]
+        public string? Nome { get; set; }
 
-    [Required]
-    [StringLength(300)]
-    public string? Descricao { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "A descrição deve ter no máximo {1} caracteres")]
+        public string? Descricao { get; set; }
 
-    [Required]
-    [Column(TypeName = "decimal(10,2)")]
-    public decimal Preco { get; set; }
+        [Required]
+        [Range(1, 10000, ErrorMessage = "O preço deve estar entre {1} e {2}")]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Preco { get; set; }
 
-    [Required]
-    [StringLength(300)]
-    public string? ImagemUrl { get; set; }
-    public float Estoque { get; set; }
-    public DateTime DataCadastro { get; set; }
-    public int CategoriaId { get; set; }
+        [Required]
+        [StringLength(300, MinimumLength = 10)]
+        public string? ImagemUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public int CategoriaId { get; set; }
 
-    [JsonIgnore]
-    public Categoria? Categoria { get; set; }
+        [JsonIgnore]
+        public Categoria? Categoria { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(this.Nome))
+            {
+                var primeiraLetra = this.Nome[0].ToString();
+                if (primeiraLetra != primeiraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("A primeira letra do produto deve ser maiúsculo",
+                    [nameof(this.Nome)]
+                    );
+                }
+            }
+            
+            if (this.Estoque <= 0)
+            {
+                yield return new ValidationResult("O estoque deve ser maior que zero",
+                [nameof(this.Estoque)]
+                );
+            }
+        }
+    }
 }
 
